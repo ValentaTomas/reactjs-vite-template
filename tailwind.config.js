7c1fe1d7a4cff68aef9454a5541633ca1c815865
Tomas Valenta <valenta.and.thomas@gmail.com>
2021-10-14 14:10:43 +0200

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.tsx'],
+  darkMode: false,
+  mode: 'jit',
+  important: true,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

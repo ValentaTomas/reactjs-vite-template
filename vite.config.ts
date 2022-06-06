fb14c3d2386a57f09a9aa4dd14a7e26d1330a5bb
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-06-06 23:48:23 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,18 @@ export default defineConfig({
   plugins: [
     reactRefresh(),
   ],
+  server: {
+    hmr: process.env.GITPOD_WORKSPACE_URL
+      ? {
+        // Due to port fowarding, we have to replace
+        // 'https' with the forwarded port, as this
+        // is the URI created by Gitpod.
+        host: process.env.GITPOD_WORKSPACE_URL.replace("https://", "3000-"),
+        protocol: "wss",
+        clientPort: 443
+      }
+      : true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
